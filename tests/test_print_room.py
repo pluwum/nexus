51994de8d5ce314f4b9ae4a447e765efaf3fea9d
Patrick Luwum <luwyxx@gmail.com>
2017-04-26 20:34:30 +0300

    Added skeleton for Print Room ests

---
+++ tests/test_print_room.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from classes.andela import Andela
+
+class TestPrintRoom(TestCase):
+	def setUp(self):
+		pass
+	def test_method_successfully_returns_room_occupants(self):
+		pass
+	def test_method_raises_exception_if_room_given_doesnt_exist():
+		pass
+	def test_method_successfully_returns_false_if_no_occupants():
+		pass
