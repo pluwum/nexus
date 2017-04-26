331e74aef99702100fe9f2aac9a853df91d8a70f
Patrick Luwum <luwyxx@gmail.com>
2017-04-26 21:29:00 +0300

    Added test for print Room functionality

--- tests/test_print_room.py
+++ tests/test_print_room.py
@@ -3,10 +3,21 @@ from classes.andela import Andela
 
 class TestPrintRoom(TestCase):
 	def setUp(self):
-		pass
+		self.andela = Andela()
+		self.andela.createRoom("Blue","office")
+		self.andela.createRoom("Yellow","livingspace")
+
+		self.andela.addPerson('patrick','fellow',True)
+		self.andela.addPerson('alvin','staff')
+		self.andela.addPerson('calvin','fellow')
+		self.andela.addPerson('Dalvin','staff')
+
+
 	def test_method_successfully_returns_room_occupants(self):
-		pass
+		office_occupants = self.andela.getRoomOccupants("Blue")
+		livingspace_occupants = self.andela.getRoomOccupants("Blue")
+
+		self.assertListEqual([True, 4, 1], [isinstance(office_occupants, list), len(office_occupants), len(livingspace_occupants)])
+
 	def test_method_raises_exception_if_room_given_doesnt_exist():
-		pass
-	def test_method_successfully_returns_false_if_no_occupants():
-		pass
+		self.assertRaises(ValueError, self.andela.getRoomOccupants, "Red")
