b1ca3e16a14f7b22a4aadecd9f1c6c89e0d243eb
Patrick Luwum <luwyxx@gmail.com>
2017-04-27 20:41:08 +0300

    Combined Fellow and Staff class into one file

--- classes/person.py
+++ classes/person.py
@@ -11,3 +11,24 @@ class Person(object):
 
 	def getRole():
 		return self.role
+
+class Staff(Person):
+
+	def __init__(self, name, identifier):
+		self.role = 'staff'
+		super(Staff, self).__init__(name, identifier, self.role)
+
+class Fellow(Person):
+	def __init__(self, name, identifier, requires_living_space):
+		self.role = 'fellow'
+		self.requires_living_space = requires_living_space
+		super(Fellow, self).__init__(name, identifier, self.role)
+
+	def allocateLivingSpace(self, room):
+		if self.requires_living_space:
+			self.living_space = room
+	def requiresLivingSpace(self):
+		if self.requires_living_space:
+			return "Y"
+		else:
+			return "N"					
